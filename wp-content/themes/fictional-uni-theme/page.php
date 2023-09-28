4f385d46dd1593a6c668b1b98e46bac696b4c4ec
Thinh Le <letienthinh0109@gmail.com>
2023-09-28 17:59:12 +0700

    dynamically showing children & check for standalone page also

--- wp-content/themes/fictional-uni-theme/page.php
+++ wp-content/themes/fictional-uni-theme/page.php
@@ -25,14 +25,35 @@ while (have_posts()) {
     }
     ?>
 
-    <!-- <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
-      <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
-      </ul>
-    </div>
- -->
+    <?php
+    $hasChild = get_pages(array(
+      'child_of' => get_the_ID()
+    ));
+    if ($theParentID or $hasChild) { ?>
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="<?php echo get_permalink($theParentID) ?>"><?php echo get_the_title($theParentID) ?></a></h2>
+        <ul class="min-list">
+          <?php
+          if ($theParentID) {
+            $findChildrenOf = $theParentID;
+          } else {
+            $findChildrenOf = get_the_ID();
+          }
+
+          wp_list_pages(array(
+            'title_li' => NULL,
+            'child_of' => $findChildrenOf,
+            'sort_column' => 'menu_order',
+            // menu_order is determined by the order config below the "Page Attribute"
+            // section on classic editor
+          ));
+          ?>
+          <!-- <li class="current_page_item"><a href="#">Our History</a></li>
+        <li><a href="#">Our Goals</a></li> -->
+        </ul>
+      </div>
+    <?php } ?>
+
     <div class="generic-content">
       <?php the_content(); ?>
     </div>
